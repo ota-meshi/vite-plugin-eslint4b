aaff1ee6796cb249f30bbafa5799eaba447ce34b
yosuke ota <otameshiyo23@gmail.com>
2025-07-04 11:01:20 +0900

    add shim

--- shim/process-shim.mjs
+++ shim/process-shim.mjs
@@ -6,3 +6,12 @@ export const stdout = {};
 export function platform() {
   return "";
 }
+export function hrtime() {
+  return [0, 0];
+}
+export function emitWarning() {
+  // No-op
+}
+export function on() {
+  // No-op
+}
